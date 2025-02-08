9a93405e3476e1282be9d3e48b4ecc5ba2b7cbca
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-02-08 06:40:06 +0000

    Completed code for DTOs

--- src/DTOs/Expense.java
+++ src/DTOs/Expense.java
@@ -1,4 +1,76 @@
 package DTOs;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Expense {
+    // Auto-increment ref: https://stackoverflow.com/questions/24305830/java-auto-increment-id
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    private int id; // auto-incremented
+    private String title;
+    private String category;
+    private double amount;
+    private Date incurred;
+
+    // if ID is already known. Used for retrieving from database
+    public Expense(int id, String title, String category, double amount, Date incurred) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.amount = amount;
+        this.incurred = incurred;
+    }
+
+    // for creating a new one. ID is incremented here
+    public Expense(String title, String category, double amount, Date incurred) {
+        this.id = count.incrementAndGet();
+        this.title = title;
+        this.category = category;
+        this.amount = amount;
+        this.incurred = incurred;
+    }
+
+    // default constructor
+    public Expense() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return incurred;
+    }
+
+    public void setDate(Date incurred) {
+        this.incurred = incurred;
+    }
 }
--- src/DTOs/Income.java
+++ src/DTOs/Income.java
@@ -1,4 +1,65 @@
 package DTOs;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Income {
+    // Auto-increment ref: https://stackoverflow.com/questions/24305830/java-auto-increment-id
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    private int id; // auto-incremented
+    private String title;
+    private double amount;
+    private Date earned;
+
+    // if ID is already known. Used for retrieving from database
+    public Income(int id, String title, double amount, Date earned) {
+        this.id = id;
+        this.title = title;
+        this.amount = amount;
+        this.earned = earned;
+    }
+
+    // for creating a new one. ID is incremented here
+    public Income(String title, double amount, Date earned) {
+        this.id = count.incrementAndGet();
+        this.title = title;
+        this.amount = amount;
+        this.earned = earned;
+    }
+
+    // default constructor
+    public Income() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return earned;
+    }
+
+    public void setDate(Date earned) {
+        this.earned = earned;
+    }
 }
