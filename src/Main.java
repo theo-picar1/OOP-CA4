eb3fc8d62c82f30dfb0dd86463ab53e3dea38f90
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-02-10 05:55:18 +0000

    Implemented totalSpent and totalEarned

--- src/Main.java
+++ src/Main.java
@@ -18,28 +18,36 @@ public class Main {
             //Expense
             System.out.println("Executing findAllExpenses()...");
             List<Expense> expenseList = IExpenseDao.findAllExpenses();
+            double totalSpent = 0;
 
             if(expenseList.isEmpty()) {
                 System.out.println("Expense table is empty! Please add some data first.");
             }
             else {
                 for(Expense expenseRow : expenseList) {
+                    totalSpent += expenseRow.getAmount();
                     System.out.println(expenseRow.toString());
                 }
             }
 
+            System.out.println("Total money spent is: €" +totalSpent);
+
             // Income
             System.out.println("\nExecuting findAllIncome()...");
             List<Income> incomeList = IIncomeDao.findAllIncome();
+            double totalEarned = 0;
 
             if(incomeList.isEmpty()) {
                 System.out.println("Income table is empty! Please add some data first.");
             }
             else {
                 for(Income incomeRow : incomeList) {
+                    totalEarned += incomeRow.getAmount();
                     System.out.println(incomeRow.toString());
                 }
             }
+
+            System.out.println("Total money earned is: €" +totalEarned);
         }
         catch (DaoException e) {
             e.printStackTrace();
